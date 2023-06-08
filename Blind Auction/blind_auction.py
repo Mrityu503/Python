dee8e571738d6c059a4d5e40619eb8f8bfa5194e
Mrityu503 <37443062+Mrityu503@users.noreply.github.com>
2023-06-08 16:49:16 +0530

    Update blind_auction.py

--- Blind Auction/blind_auction.py
+++ Blind Auction/blind_auction.py
@@ -1,18 +1,26 @@
-import art
-from replit import clear
-#HINT: You can call clear() to clear the output in the console.
-print("Welcome to secret auction program!")
-print(art.logo)
-go_again = "yes"
-auction = {}
-while go_again == "yes":
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
   name = input("What is your name?: ")
-  bid = input("What's your bid?: $")
-  auction[name] = bid
-  go_again = input("Are there any other bidders? Type 'yes' or 'no'.")  
-  clear()
-winner = max(auction, key = auction.get)
-print(f"The winner is {winner} with a bid of ${auction[winner]} ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
 
 #Alternate for clear other than replit
 # from os import system
